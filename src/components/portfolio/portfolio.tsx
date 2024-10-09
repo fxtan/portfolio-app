85fc0799a65ac281a1bd68be99c99258f3956dfc
fxtan <charaevairina998@gmail.com>
2024-10-09 20:01:42 +0300

    закончила десктопный портфолио

--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Title from '../../shared/title/title';
 import { myPortfolio } from '../../constants/portfolio';
+import style from './style.module.scss';
 
 const Portfolio = () => {
   return (
     <div>
       <Title type={'h1'} text={'Portfolio'} />
+      {myPortfolio.map((portfolio, index) => {
+          return (
+            <div>
+              
+              <img className={style.image} src={portfolio.image} alt="" />
+              <h2 className={style.title}>{portfolio.name}</h2>
+            </div>
+          );
+        })}
     </div>
   )
 }
